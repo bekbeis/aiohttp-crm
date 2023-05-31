1de2cf1276a1db414442671aad4e6d6e305762ff
Bekzat Skakov <bekzat.beis@gmail.com>
2023-05-31 13:01:03 +0600

    ListUsers and GetUser views added

--- app/crm/routes.py
+++ app/crm/routes.py
@@ -1,8 +1,9 @@
 import typing
 
 # Условно: если идет проверка типов, то импортируем. В обратном случае, нет.
-# Почему вообще это надо? Мы импортируем Application и напрямую, и цепочкой импортов через AddUserView.
-# Программе (?) может быть непонятно, что и откуда тянуть первым.
+# Почему вообще это надо? Мы импортируем Application и напрямую, и цепочкой
+# импортов через AddUserView. Программе (?) может быть непонятно,
+# что и откуда тянуть первым.
 if typing.TYPE_CHECKING:
     from app.web.app import Application
 
@@ -15,9 +16,11 @@ if typing.TYPE_CHECKING:
 
 
 def setup_routes(app: "Application"):
-    from app.crm.views import AddUserView
+    from app.crm.views import AddUserView, ListUsersView, GetUserView
     # Если на страницу /page_name приходит GET-запрос,
     # то он попадет в нашу функцию function_name из views.py
     # выгдлядит это примерно так:
     # app.router.add_get('/page_name', function_name)
     app.router.add_view('/add_user', AddUserView)
+    app.router.add_view('/list_users', ListUsersView)
+    app.router.add_view('/get_user', GetUserView)
--- app/crm/views.py
+++ app/crm/views.py
@@ -1,7 +1,7 @@
 import uuid
 
 from aiohttp.web_response import json_response
-
+from aiohttp.web_exceptions import HTTPNotFound
 from app.web.app import View
 from app.crm.models import User
 
@@ -9,7 +9,26 @@ from app.crm.models import User
 class AddUserView(View):
     async def post(self):
         data = await self.request.json()
-        user = User(_id=uuid.uuid4(), email=data['email'])
+        user = User(id_=uuid.uuid4(), email=data['email'])
         await self.request.app.crm_accessor.add_user(user)
-        print(self.request.app.database)
         return json_response(data={'status': 'ok'})
+
+
+class ListUsersView(View):
+    async def get(self):
+        users = await self.request.app.crm_accessor.list_users()
+        raw_users = [{'email:': user.email,
+                      'id': str(user.id_)} for user in users]
+        return json_response(data={'status': 'ok', 'users': raw_users})
+
+
+class GetUserView(View):
+    async def get(self):
+        # Чтобы получить информацию из URL, нужно использовать
+        # query параметры.
+        user_id = self.request.query['id']
+        user = await self.request.app.crm_accessor.get_user(uuid.UUID(user_id))
+        if user:
+            return json_response(data={'status': 'ok', 'user': {'email': user.email, 'id': str(user.id_)}})
+        else: 
+            raise HTTPNotFound
